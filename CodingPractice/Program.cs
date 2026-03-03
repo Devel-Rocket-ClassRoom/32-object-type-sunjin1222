f35b4ace0eb531277421fe7fa3a7327f73163787
kim sunjin <rlatjswls14@gmail.com>
2026-03-03 18:10:16 +0900

    제출

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,4 +1,233 @@
 using System;
+using System.Threading;
+/*
+// 1. object 타입의 기본 개념
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+object strin1 = 42;
+object int1 = 3.14;
+object double1 = "Hello";
+object bool1= true;
+Console.WriteLine(strin1);
+Console.WriteLine(int1);
+Console.WriteLine(double1);
+Console.WriteLine(bool1);
+
+// 2. object 타입의 활용
+
+Stack stack = new Stack();
+
+stack.Push("sausage");
+string s = (string)stack.Pop();
+Console.WriteLine(s);
+
+stack.Push(3);
+int three = (int)stack.Pop();
+Console.WriteLine(three);
+
+class Stack
+{
+    private int _position;
+    private object[] _data = new object[10];
+
+    public void Push(object obj)
+    {
+        _data[_position++] = obj;
+    }
+
+    public object Pop()
+    {
+        return _data[--_position];
+    }
+}
+
+
+
+
+// 3-1. 박싱
+
+int i = 1234;
+object o = i;
+Console.WriteLine(o);
+
+
+// 3-2. 언박싱
+object o = 1234;
+int i = (int)o;
+Console.WriteLine(i);
+
+// 4-1. 값 타입을 object에 할당
+int x = 42;
+object o = x;
+Console.WriteLine(o);
+
+
+// 4-2. 값 타입을 인터페이스 타입에 할당
+Point p = new Point { X = 10, Y = 20 };
+IDisplayable d = p; 
+Console.WriteLine(d.GetType().Name);
+
+interface IDisplayable
+{
+}
+
+struct Point : IDisplayable
+{
+    public int X;
+    public int Y;
+}
+
+
+// 5. 박싱의 특징
+int i = 3;
+object o = i;
+i = 5;
+
+
+
+Console.WriteLine($"원본: {i}");
+Console.WriteLine($"박싱된 값:{o}");
+
+// 6. 언박싱 시 주의사항
+object o = 42;
+int i = (int)o;
+Console.WriteLine($"언박싱 성공: {i}");
+try
+{
+    long l = (long)i;
+}
+catch (InvalidCastException)
+{
+    Console.WriteLine("잘못된 타입으로 언박싱 시도");
+}
+
+int temp = (int)i;   
+long correct = temp;     
+Console.WriteLine($"올바른 변환: {correct}");
+
+
+
+// 7-1. 박싱/언박싱 오버헤드 비교
+
+
+int sum1 = 0;
+for (int i = 0; i < 1000; i++)
+{
+    object boxed = i;      
+    sum1 += (int)boxed;  
+}
+Console.WriteLine($"박싱 사용: {sum1}");
+
+int sum2 = 0;
+for (int i = 0; i < 1000; i++)
+{
+    sum2 += i; 
+}
+Console.WriteLine($"직접 처리: {sum2}");
+
+
+
+// 7-2. 제네릭으로 박싱 피하기
+
+
+// 8-1. GetType() 메서드
+int a = 45;
+string b = "안녕하세요";
+char c = 'A';
+double d = 3.14159;
+object e = new object();
+
+Console.WriteLine($"직접 처리: {a.GetType()}");
+Console.WriteLine($"직접 처리: {b.GetType()}");
+Console.WriteLine($"직접 처리: {c.GetType()}");
+Console.WriteLine($"직접 처리: {d.GetType()}");
+Console.WriteLine($"직접 처리: {e.GetType()}");
+
+
+// 8-2. typeof 연산자
+
+Console.WriteLine(typeof(int));
+Console.WriteLine(typeof(string));
+Console.WriteLine(typeof(double));
+
+// 8-3. GetType()과 typeof() 비교
+Point p = new Point { X = 10, Y = 20 };
+
+Console.WriteLine(p.GetType().Name);            
+Console.WriteLine(typeof(Point).Name);            
+Console.WriteLine(p.GetType() == typeof(Point)); 
+Console.WriteLine(p.X.GetType().Name);            
+Console.WriteLine(p.Y.GetType().FullName);
+
+class Point
+{
+    public int X;
+    public int Y;
+
+}
+
+// 9-1. 기본 동작
+
+int number = 42;
+double pi = 3.14;
+bool flag = true;
+
+Console.WriteLine(number.ToString()); 
+Console.WriteLine(pi.ToString()); 
+Console.WriteLine(flag.ToString());
+
+
+
+// 9-2. ToString() 재정의하기
+Panda p = new Panda { Name = "Petey" };
+Console.WriteLine(p);
+Console.WriteLine(p.ToString());
+
+class Panda
+{
+    public string Name;
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+
+
+// 9-3. 더 상세한 ToString() 구현
+Player player = new Player { Name = "Hero", Level = 10, Health = 100 };
+Console.WriteLine(player);
+
+class Player
+{
+    public string Name;
+    public int Level;
+    public int Health;
+
+    public override string ToString()
+    {
+        return $"Player {{ Name = {Name}, Level = {Level}, Health = {Health} }}";
+    }
+}
+
+*/
+// 10. object 클래스의 멤버들
+
+
+string s1 = "Hello";
+string s2 = "Hello";
+string s3 = s1;
+
+Console.WriteLine($"s1.Equals(s2): {s1.Equals(s2)}");                   
+Console.WriteLine($"ReferenceEquals(s1, s2): {object.ReferenceEquals(s1, s2)}");  
+Console.WriteLine($"ReferenceEquals(s1, s3): {object.ReferenceEquals(s1, s3)}");  
+
+
+object obj1 = new object();
+object obj2 = new object();
+object obj3 = obj1;
+
+Console.WriteLine($"obj1.Equals(obj2): {obj1.Equals(obj2)}");         
+Console.WriteLine($"ReferenceEquals(obj1, obj2): {object.ReferenceEquals(obj1, obj2)}");
+Console.WriteLine($"ReferenceEquals(obj1, obj3): {object.ReferenceEquals(obj1, obj3)}"); 
+
--- GameItem/Program.cs
+++ GameItem/Program.cs
@@ -1,4 +1,53 @@
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+class Item
+{
+    private string Name;
+
+    private int Price;
+
+    public void ToString()
+    { 
+    
+    }
+
+}
+
+class Weapon : Item
+{
+    private int Damage;
+    public void ToString()
+    { 
+    
+    }
+}
+class Potion : Item
+{
+    private int HealAmount;
+    public void ToString()
+    { 
+    
+    }
+}
+
+class Inventory
+{
+     private object[] item=new object[10];
+
+
+     public void AddItem(object item)
+    {
+
+    }
+   
+
+    public void ShowInventory()
+    { 
+    
+    }
+
+}
+
+
+
