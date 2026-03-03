a7dafc58c022e4b60345ba98848340a7c4c1c327
kim sunjin <rlatjswls14@gmail.com>
2026-03-04 00:57:45 +0900

    제출

--- DataPrinter/Program.cs
+++ DataPrinter/Program.cs
@@ -1,4 +1,84 @@
 using System;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+using System;
+
+Console.WriteLine("=== 데이터 출력기 ===");
+
+object[] data =
+{
+      42, 3.14, "Hello", true,
+      100, "World", false, 2.718
+};
+
+Console.WriteLine("[전체 데이터 출력]");
+PrintAll(data);
+
+Console.WriteLine("[타입별 통계]");
+PrintStatistics(data);
+
+
+
+static void PrintData(object data)
+    {
+        string typeName = data.GetType().Name;
+
+        if (typeName == "Int32")
+        {
+            Console.WriteLine($"정수: {data}");
+        }
+        else if (typeName == "Double")
+        {
+            Console.WriteLine($"실수: {(double)data:F2}");
+        }
+        else if (typeName == "String")
+        {
+            string str = (string)data;
+            Console.WriteLine($"문자열: \"{str}\" (길이: {str.Length})");
+        }
+        else if (typeName == "Boolean")
+        {
+            bool b = (bool)data;
+            string result = b ? "참" : "거짓";
+            Console.WriteLine($"논리값: {b} → {result}");
+        }
+        else
+        {
+            Console.WriteLine($"알 수 없는 타입: {typeName} - {data}");
+        }
+    }
+
+    static void PrintAll(object[] data)
+    {
+        foreach (object item in data)
+        {
+            PrintData(item);
+        }
+    }
+
+    static void PrintStatistics(object[] data)
+    {
+        int intCount = 0;
+        int doubleCount = 0;
+        int stringCount = 0;
+        int boolCount = 0;
+
+        foreach (object item in data)
+        {
+            string typeName = item.GetType().Name;
+
+            if (typeName == "Int32")
+                intCount++;
+            else if (typeName == "Double")
+                doubleCount++;
+            else if (typeName == "String")
+                stringCount++;
+            else if (typeName == "Boolean")
+                boolCount++;
+        }
+
+        Console.WriteLine($"정수: {intCount}개");
+        Console.WriteLine($"실수: {doubleCount}개");
+        Console.WriteLine($"문자열: {stringCount}개");
+        Console.WriteLine($"논리값: {boolCount}개");
+    }
+
--- GameItem/Program.cs
+++ GameItem/Program.cs
@@ -1,53 +1,110 @@
 using System;
 
-// README.md를 읽고 코드를 작성하세요.
+
+Console.WriteLine("=== 인벤토리 시스템 테스트 ===\n");
+
+Weapon sword = new Weapon("불꽃 검", 500, 25);
+Weapon bow = new Weapon("얼음 활", 450, 20);
+Potion hpPotion = new Potion("체력 물약", 50, 100);
+Potion mpPotion = new Potion("마나 물약", 80, 50);
+Potion mpPotion2= new Potion("고급마나 물약", 120, 150);
+Weapon Hammer = new Weapon("얼음 망치", 850, 100);
+
+Inventory inventory = new Inventory();
+
+inventory.AddItem(sword);
+inventory.AddItem(bow);
+inventory.AddItem(hpPotion);
+inventory.AddItem(mpPotion);
+inventory.AddItem(mpPotion2);
+inventory.AddItem(mpPotion2);
+inventory.AddItem(Hammer);
+
+Console.WriteLine("[인벤토리 내용]");
+inventory.ShowInventory();
+
+Console.WriteLine("=== 타입 확인 테스트 ===");
+
+Console.WriteLine($"sword의 타입: {sword.GetType().Name}");
+Console.WriteLine($"sword.GetType() == typeof(Weapon): {sword.GetType() == typeof(Weapon)}");
+Console.WriteLine($"sword.GetType() == typeof(Item): {sword.GetType() == typeof(Item)}");
+Console.WriteLine($"sword is Item: {sword is Item}");
+
 class Item
 {
-    private string Name;
+    public string Name;
 
-    private int Price;
+    public int Price; 
 
-    public void ToString()
-    { 
-    
+    public Item(string name, int price)
+    {
+        Name = name;
+        Price = price;
     }
 
+    public override string ToString()
+    {
+        return $"Item {{ Name = {Name}, Price = {Price}}}";
+    }
 }
 
 class Weapon : Item
 {
-    private int Damage;
-    public void ToString()
-    { 
-    
+    public int Damage;
+
+    public Weapon(string name, int price, int damage): base(name, price)
+    {
+        Damage = damage;
+    }
+
+    public override string ToString()
+    {
+        return $"Weapon Name = {{{Name}, Price = {Price}, Damage = {Damage}}}";
     }
 }
+
 class Potion : Item
 {
-    private int HealAmount;
-    public void ToString()
-    { 
-    
+    public int HealAmount;
+
+    public Potion(string name, int price, int healAmount) : base(name, price)
+    {
+        HealAmount = healAmount;
+    }
+
+    public override string ToString()
+    {
+        return $"Potion Name = {{{Name}, Price = {Price}, HealAmount = {HealAmount}}} ";
     }
 }
 
 class Inventory
 {
-     private object[] item=new object[10];
+    private object[] items = new object[10];
+    private int index = 0;
 
-
-     public void AddItem(object item)
+    public void AddItem(object item)
     {
-
+        if (index < items.Length)
+        {
+            items[index] = item;
+            index++;
+        }
+        else
+        {
+            Console.WriteLine("인벤토리가 가득 찼습니다.");
+        }
     }
-   
 
     public void ShowInventory()
-    { 
-    
+    {
+        for (int i = 0; i < index; i++)
+        {
+            object item = items[i];
+            Console.WriteLine($"슬롯 {i + 1}: {item} [{item.GetType().Name}]");
+        }
     }
-
 }
 
 
-
+  
